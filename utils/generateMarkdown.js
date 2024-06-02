fb7eb6c09bb094ac6a80ef2abe0f098a7146c84d
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-06-02 18:59:51 -0400

    License markdown
    -Addded function to render the license section of README
    -Adde function to generate markdown forr README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,4 +18,67 @@ function renderLicenseBadge(license) {
     }
   }
 
+  // Function to render the license link
+function renderLicenseLink(license) {
+    if (!license) {
+        return "";
+    }
   
+    switch (license) {
+        case "MIT License":
+            return "https://opensource.org/licenses/MIT";
+        case "Apache License 2.0":
+            return "https://opensource.org/licenses/Apache-2.0";
+        case "GNU General Puplic License v3.0":
+            return "https://www.gnu.org/licenses/gpl-3.0";
+        case "Mozilla Public License 2.0":
+            return "https://www.mozilla.org/en-US/MPL/2.0/";
+        default:
+            return "";
+    }
+  }
+
+  // Function to render the license section of README
+function renderLicenseSection(license) {
+    if (!license) {
+        return "";
+    }
+  
+    return `## License:
+    
+    This project is licensed under the ${license} license.
+    
+    ${renderLicenseBadge(license)};
+    ${renderLicenseLink(license)}`
+  }
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+    
+    ${renderLicenseBadge(data.license)}
+    
+    ## Description:
+    ${data.description}
+    
+    ## Table of Contents:
+    - [Installation Instructions](#Installation-Instructions)
+    - [Usage Information](#Usage-Information)
+    - [Contribution Guidelines](#Contribution-Guidelines)
+    - [Test Instructions](#Test-Instructions)
+    - [License](#License)
+    - [Questions](#Questions)
+  
+    ## Installation Instructions:
+    ${data.installation}
+    
+    ## Usage Instructions:
+    ${data.usage}
+    
+    ## Contribution Guidelines:
+    ${data.contribution}
+    
+    ## Test Instructions:
+    ${data.test}
+    
+    ${renderLicenseSection(data.license)}
